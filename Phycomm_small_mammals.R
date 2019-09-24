ff8184ea0ff309d24e5a581a5b5e0f7f781eea62
Meng Chen <meng.chen03@gmail.com>
2019-09-24 17:17:41 -0400

    update the R codes
    get rid of some unconvential names

--- Phycomm_small_mammals.R
+++ Phycomm_small_mammals.R
@@ -3,15 +3,16 @@
 ## Phylogenetic Community Structure of small-bodied mammal communities ##
 #########################################################################
 
+require(ape)
+require(picante)
+
 ## Reading data
-occur<-read.csv("PCS Data 5Feb2016.csv",header=T, sep=",")
+occur<-read.csv("PCS_Data_5Feb2016.csv",header=T, sep=",")
 
 # create community by species occurrence matrix
-
 spec_occur<-xtabs(~occur$Community+occur$Species)
 
 # get rid of any values that might be greater than one
-
 for(i in 1:nrow(spec_occur)){
   for(j in 1:ncol(spec_occur)){
     if(spec_occur[i,j]>1){
@@ -20,35 +21,30 @@ for(i in 1:nrow(spec_occur)){
   }
 }
 
-write.csv(spec_occur,"Species by community matrix.csv")
+write.csv(spec_occur,"Species_by_community_matrix.csv")
 
-spec_occur<-read.csv("Species by community matrix.csv",header=T,row.names=1)
-
+spec_occur<-read.csv("Species_by_community_matrix.csv",header=T, row.names=1)
 
 # calculate phycomm
-require(ape)
-
 fritz_tree<-read.nexus("Fritz_tree.nex")
 spec_Meng<-occur$Species
 
 fritz_tree1<-fritz_tree[[1]]
-taxa.in.both<-intersect(fritz_tree1$tip.label, spec_Meng) # 294 species
-mismatches<-setdiff(spec_Meng,taxa.in.both)
+taxa_in_both<-intersect(fritz_tree1$tip.label, spec_Meng) # 294 species
+mismatches<-setdiff(spec_Meng,taxa_in_both)
 
-dropset<-setdiff(fritz_tree1$tip.label,taxa.in.both)
-prunedtree<-drop.tip(fritz_tree1,tip=dropset)
+drop_set<-setdiff(fritz_tree1$tip.label, taxa_in_both)
+pruned_tree<-drop.tip(fritz_tree1, tip=drop_set)
 
-spec_occur<-spec_occur[,prunedtree$tip.label]
+spec_occur<-spec_occur[, pruned_tree$tip.label]
 
 ## Alternatives
-occur<-read.csv("Chen_Wilson_Species_2015.csv",header=T, sep=",")
+occur<-read.csv("species_names.csv",header=T, sep=",")
 
 # create community by species occurrence matrix
-
 spec_occur<-xtabs(~occur$Community+occur$Species)
 
 # get rid of any values that might be greater than one
-
 for(i in 1:nrow(spec_occur)){
   for(j in 1:ncol(spec_occur)){
     if(spec_occur[i,j]>1){
@@ -58,42 +54,34 @@ for(i in 1:nrow(spec_occur)){
 }
 
 # calculate phycomm
-require(ape)
-
 fritz_tree<-read.nexus("Fritz_tree.nex")
 spec_Meng<-occur$Species
 
 fritz_tree1<-fritz_tree[[1]]
-taxa.in.both<-intersect(fritz_tree1$tip.label, spec_Meng) # 294 species
-mismatches<-setdiff(spec_Meng,taxa.in.both)
+taxa_in_both<-intersect(fritz_tree1$tip.label, spec_Meng) # 294 species
+mismatches<-setdiff(spec_Meng,taxa_in_both)
 
-dropset<-setdiff(fritz_tree1$tip.label,taxa.in.both)
-prunedtree<-drop.tip(fritz_tree1,tip=dropset)
+drop_set<-setdiff(fritz_tree1$tip.label, taxa_in_both)
+pruned_tree<-drop.tip(fritz_tree1, tip=drop_set)
 
 spec_occur<-spec_occur[,prunedtree$tip.label]
 
 # Net relatedness Index
-phydist<- cophenetic(prunedtree)
-
-require(picante)
-
-system.time(ses.mpd.result <- ses.mpd(spec_occur,phydist,null.model = "taxa.labels",
+phy_dist<- cophenetic(pruned_tree)
+system.time(ses_mpd_result <- ses.mpd(spec_occur, phy_dist, null.model = "taxa.labels",
                                       abundance.weighted = FALSE, runs = 100))
-
-NRI<--1*(ses.mpd.result$mpd.obs.z)
+NRI<--1*(ses_mpd_result$mpd.obs.z)
 
 community<-unique(occur$NO)
 
-community_nri<-data.frame(cbind(community,NRI))
+community_nri<-data.frame(cbind(community, NRI))
 
 plot(community_nri$community,community_nri$NRI)
 
-
-# By climate code
+# By different climte
 spec_occur<-xtabs(~occur$Climate.Code+occur$Species)
 
 # get rid of any values that might be greater than one
-
 for(i in 1:nrow(spec_occur)){
   for(j in 1:ncol(spec_occur)){
     if(spec_occur[i,j]>1){
@@ -102,37 +90,29 @@ for(i in 1:nrow(spec_occur)){
   }
 }
 
-write.csv(spec_occur,"Species by climate code matrix.csv")
+write.csv(spec_occur,"Species_by_climate_code_matrix.csv")
 
 # calculate phycomm
-
-require(ape)
-
 fritz_tree<-read.nexus("Fritz tree.nex")
 
 fritz_tree1<-fritz_tree[[1]]
-taxa.in.both<-intersect(fritz_tree1$tip.label, colnames(spec_occur)) # 266 species
-mismatches<-setdiff(colnames(spec_occur),taxa.in.both)
+taxa_in_both<-intersect(fritz_tree1$tip.label, colnames(spec_occur)) # 266 species
+mismatches<-setdiff(colnames(spec_occur), taxa_in_both)
 
-dropset<-setdiff(fritz_tree1$tip.label,taxa.in.both)
-prunedtree<-drop.tip(fritz_tree1,tip=dropset)
+drop_set<-setdiff(fritz_tree1$tip.label, taxa_in_both)
+pruned_tree<-drop.tip(fritz_tree1, tip=drop_set)
 
-spec_occur<-spec_occur[,prunedtree$tip.label]
+spec_occur<-spec_occur[,pruned_tree$tip.label]
 
 # Net relatedness Index
-phydist<- cophenetic(prunedtree)
-
-require(picante)
-
-system.time(ses.mpd.result <- ses.mpd(spec_occur,phydist,null.model = "taxa.labels",
+phy_dist<- cophenetic(pruned_tree)
+system.time(ses_mpd_result <- ses.mpd(spec_occur, phy_dist, null.model = "taxa.labels",
                                       abundance.weighted = FALSE, runs = 1000))
 
-NRI<--1*(ses.mpd.result$mpd.obs.z)
+NRI<--1*(ses_mpd_result$mpd.obs.z)
 
 community<-unique(occur$Climate.Code)
 
 community_nri<-data.frame(cbind(community,NRI))
 
 plot(community_nri$community,community_nri$NRI)
-
-##
